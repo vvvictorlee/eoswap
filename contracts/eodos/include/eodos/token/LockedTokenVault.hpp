b91d6bfcdf7a3852d22b763882f7e5e2dba84ab1
lisheng <lisheng@zhidaoauto.com>
2020-10-27 19:30:02 +0800

    add src

--- contracts/eodos/include/eodos/token/LockedTokenVault.hpp
+++ contracts/eodos/include/eodos/token/LockedTokenVault.hpp
@@ -5,16 +5,14 @@
 
 */
 
-#pragma once 
- #include <common/defines.hpp>
+#pragma once
+#include <common/defines.hpp>
 
-
-#include <eodos/lib/SafeMath.hpp>
+#include <eodos/intf/IERC20.hpp>
 #include <eodos/lib/DecimalMath.hpp>
 #include <eodos/lib/Ownable.hpp>
 #include <eodos/lib/SafeERC20.hpp>
-#include <eodos/intf/IERC20.hpp>
-
+#include <eodos/lib/SafeMath.hpp>
 
 /**
  * @title LockedTokenVault
@@ -23,147 +21,117 @@
  * @notice Lock Token and release it linearly
  */
 #ifdef LockedTokenVault
-class LockedTokenVault : public  Ownable {
+class LockedTokenVault : public Ownable {
  public:
-      
    // ============ Modifiers ============
-   
-    void beforeStartRelease() {
-        require(block.timestamp < _START_RELEASE_TIME_, "RELEASE START");
-        
-    }
 
-    void afterStartRelease() {
-        require(block.timestamp >= _START_RELEASE_TIME_, "RELEASE NOT START");
-        
-    }
+   void beforeStartRelease() { require(block.timestamp < _START_RELEASE_TIME_, "RELEASE START"); }
 
-    void distributeNotFinished() {
-        require(!_DISTRIBUTE_FINISHED_, "DISTRIBUTE FINISHED");
-        
-    }
+   void afterStartRelease() { require(block.timestamp >= _START_RELEASE_TIME_, "RELEASE NOT START"); }
 
-    // ============ Init Functions ============
+   void distributeNotFinished() { require(!_DISTRIBUTE_FINISHED_, "DISTRIBUTE FINISHED"); }
 
-    LockedTokenVault(
-        address _token,
-        uint256 _startReleaseTime,
-        uint256 _releaseDuration,
-        uint256 _cliffRate
-    ) {
-        _TOKEN_ = _token;
-        _START_RELEASE_TIME_ = _startReleaseTime;
-        _RELEASE_DURATION_ = _releaseDuration;
-        _CLIFF_RATE_ = _cliffRate;
-    }
+   // ============ Init Functions ============
 
-    void  deposit(uint256 amount) {
-onlyOwner();
-        _tokenTransferIn(_OWNER_, amount);
-        _UNDISTRIBUTED_AMOUNT_ = _UNDISTRIBUTED_AMOUNT_.add(amount);
-    }
+   LockedTokenVault(address _token, uint256 _startReleaseTime, uint256 _releaseDuration, uint256 _cliffRate) {
+      _TOKEN_              = _token;
+      _START_RELEASE_TIME_ = _startReleaseTime;
+      _RELEASE_DURATION_   = _releaseDuration;
+      _CLIFF_RATE_         = _cliffRate;
+   }
 
-    void  withdraw(uint256 amount) {
-onlyOwner();
-        _UNDISTRIBUTED_AMOUNT_ = _UNDISTRIBUTED_AMOUNT_.sub(amount);
-        _tokenTransferOut(_OWNER_, amount);
-    }
+   void deposit(uint256 amount) {
+      onlyOwner();
+      _tokenTransferIn(_OWNER_, amount);
+      _UNDISTRIBUTED_AMOUNT_ = _UNDISTRIBUTED_AMOUNT_.add(amount);
+   }
 
-    void  finishDistribute() {
-onlyOwner();
-        _DISTRIBUTE_FINISHED_ = true;
-    }
+   void withdraw(uint256 amount) {
+      onlyOwner();
+      _UNDISTRIBUTED_AMOUNT_ = _UNDISTRIBUTED_AMOUNT_.sub(amount);
+      _tokenTransferOut(_OWNER_, amount);
+   }
 
-    // ============ For Owner ============
+   void finishDistribute() {
+      onlyOwner();
+      _DISTRIBUTE_FINISHED_ = true;
+   }
 
-    void grant(address[]  holderList, uint256[]  amountList) {
-onlyOwner();       
- require(holderList.length == amountList.length, "batch grant length not match");
-        uint256 amount = 0;
-        for (uint256 i = 0; i < holderList.length; ++i) {
-            // for saving gas, no 
-            amount = amount.add(amountList[i]);
-        }
-        _UNDISTRIBUTED_AMOUNT_ = _UNDISTRIBUTED_AMOUNT_.sub(amount);
-    }
+   // ============ For Owner ============
 
-    void  recall(address holder) {
-onlyOwner();
- distributeNotFinished();
-        _UNDISTRIBUTED_AMOUNT_ = _UNDISTRIBUTED_AMOUNT_.add(originBalances[holder]).sub(
-            claimedBalances[holder]
-        );
-        originBalances[holder] = 0;
-        claimedBalances[holder] = 0;
-    }
+   void grant(address[] holderList, uint256[] amountList) {
+      onlyOwner();
+      require(holderList.length == amountList.length, "batch grant length not match");
+      uint256 amount = 0;
+      for (uint256 i = 0; i < holderList.length; ++i) {
+         // for saving gas, no
+         amount = amount.add(amountList[i]);
+      }
+      _UNDISTRIBUTED_AMOUNT_ = _UNDISTRIBUTED_AMOUNT_.sub(amount);
+   }
 
-    // ============ For Holder ============
+   void recall(address holder) {
+      onlyOwner();
+      distributeNotFinished();
+      _UNDISTRIBUTED_AMOUNT_  = _UNDISTRIBUTED_AMOUNT_.add(originBalances[holder]).sub(claimedBalances[holder]);
+      originBalances[holder]  = 0;
+      claimedBalances[holder] = 0;
+   }
 
-    void  transferLockedToken(address to) {
-        originBalances[to] = originBalances[to].add(originBalances[getMsgSender()]);
-        claimedBalances[to] = claimedBalances[to].add(claimedBalances[getMsgSender()]);
+   // ============ For Holder ============
 
-        originBalances[getMsgSender()] = 0;
-        claimedBalances[getMsgSender()] = 0;
-    }
+   void transferLockedToken(address to) {
+      originBalances[to]  = originBalances[to].add(originBalances[getMsgSender()]);
+      claimedBalances[to] = claimedBalances[to].add(claimedBalances[getMsgSender()]);
 
-    void  claim() {
-        uint256 claimableToken = getClaimableBalance(getMsgSender());
-        _tokenTransferOut(getMsgSender(), claimableToken);
-        claimedBalances[getMsgSender()] = claimedBalances[getMsgSender()].add(claimableToken);
-        emit Claim(
-            getMsgSender(),
-            originBalances[getMsgSender()],
-            claimedBalances[getMsgSender()],
-            claimableToken
-        );
-    }
+      originBalances[getMsgSender()]  = 0;
+      claimedBalances[getMsgSender()] = 0;
+   }
 
-    // ============ View ============
+   void claim() {
+      uint256 claimableToken = getClaimableBalance(getMsgSender());
+      _tokenTransferOut(getMsgSender(), claimableToken);
+      claimedBalances[getMsgSender()] = claimedBalances[getMsgSender()].add(claimableToken);
+      emit Claim(getMsgSender(), originBalances[getMsgSender()], claimedBalances[getMsgSender()], claimableToken);
+   }
 
-    bool  isReleaseStart() {
-        return block.timestamp >= _START_RELEASE_TIME_;
-    }
+   // ============ View ============
 
-    uint256  getOriginBalance(address holder) {
-        return originBalances[holder];
-    }
+   bool isReleaseStart() { return block.timestamp >= _START_RELEASE_TIME_; }
 
-    uint256  getClaimedBalance(address holder) {
-        return claimedBalances[holder];
-    }
+   uint256 getOriginBalance(address holder) { return originBalances[holder]; }
 
-    uint256  getClaimableBalance(address holder) {
-        uint256 remainingToken = getRemainingBalance(holder);
-        return originBalances[holder].sub(remainingToken).sub(claimedBalances[holder]);
-    }
+   uint256 getClaimedBalance(address holder) { return claimedBalances[holder]; }
 
-    uint256  getRemainingBalance(address holder) {
-        uint256 remainingRatio = getRemainingRatio(block.timestamp);
-        return DecimalMath.mul(originBalances[holder], remainingRatio);
-    }
+   uint256 getClaimableBalance(address holder) {
+      uint256 remainingToken = getRemainingBalance(holder);
+      return originBalances[holder].sub(remainingToken).sub(claimedBalances[holder]);
+   }
 
-    uint256  getRemainingRatio(uint256 timestamp) {
-        if (timestamp < _START_RELEASE_TIME_) {
-            return DecimalMath.ONE;
-        }
-        uint256 timePast = timestamp.sub(_START_RELEASE_TIME_);
-        if (timePast < _RELEASE_DURATION_) {
-            uint256 remainingTime = _RELEASE_DURATION_.sub(timePast);
-            return DecimalMath.ONE.sub(_CLIFF_RATE_).mul(remainingTime).div(_RELEASE_DURATION_);
-        } else {
-            return 0;
-        }
-    }
+   uint256 getRemainingBalance(address holder) {
+      uint256 remainingRatio = getRemainingRatio(block.timestamp);
+      return DecimalMath.mul(originBalances[holder], remainingRatio);
+   }
 
-    // ============ Internal Helper ============
+   uint256 getRemainingRatio(uint256 timestamp) {
+      if (timestamp < _START_RELEASE_TIME_) {
+         return DecimalMath.ONE;
+      }
+      uint256 timePast = timestamp.sub(_START_RELEASE_TIME_);
+      if (timePast < _RELEASE_DURATION_) {
+         uint256 remainingTime = _RELEASE_DURATION_.sub(timePast);
+         return DecimalMath.ONE.sub(_CLIFF_RATE_).mul(remainingTime).div(_RELEASE_DURATION_);
+      } else {
+         return 0;
+      }
+   }
 
-    void  _tokenTransferIn(address from, uint256 amount) {
-        IERC20(_TOKEN_).safeTransferFrom(from, address(this), amount);
-    }
+   // ============ Internal Helper ============
 
-    void  _tokenTransferOut(address to, uint256 amount) {
-        IERC20(_TOKEN_).safeTransfer(to, amount);
-    }
+   void _tokenTransferIn(address from, uint256 amount) {
+      IERC20(_TOKEN_).safeTransferFrom(from, address(this), amount);
+   }
+
+   void _tokenTransferOut(address to, uint256 amount) { IERC20(_TOKEN_).safeTransfer(to, amount); }
 };
 #endif
---
+++ contracts/eodos/include/storage/ZooTable.hpp
@@ -0,0 +1,64 @@
+
+#pragma once
+#pragma once
+#include <common/defines.hpp>
+#include <storage/LibTable.hpp>
+#include <storage/ImplTable.hpp>
+
+struct DODOEthProxyStore {
+   address _DODO_ZOO_;
+   extended_symbol _WETH_;
+   EOSLIB_SERIALIZE(DODOEthProxyStore, (_DODO_ZOO_)(_WETH_))
+};
+
+struct QuoteToken2Dodo {
+   std::map<namesym, name> q2d;
+   EOSLIB_SERIALIZE(QuoteToken2Dodo, (q2d))
+};
+
+struct DODOStore {
+   DoStore                store;
+   DODOTokenStore     lpstore;
+   OwnableStore         initownable;
+   ReentrancyGuardStore guard;
+   EOSLIB_SERIALIZE(DODOStore, (store)(lpstore)(initownable)(guard))
+};
+
+struct [[eosio::table("dodo"), eosio::contract("eodos")]] DODOStorage {
+   std::map<name, DODOStore>    dodos;
+   EOSLIB_SERIALIZE(DODOStorage, (dodos))
+};
+
+typedef eosio::singleton<"dodo"_n, DODOStorage> DODOStorageSingleton;
+
+
+struct [[eosio::table("proxy"), eosio::contract("eodos")]] ProxyStorage {
+   DODOEthProxyStore proxy;
+   ReentrancyGuardStore guard;
+  
+   EOSLIB_SERIALIZE(
+       ProxyStorage, (proxy)(guard))
+};
+
+typedef eosio::singleton<"proxy"_n, ProxyStorage> ProxyStorageSingleton;
+
+
+
+struct [[eosio::table("zoo"), eosio::contract("eodos")]] ZooStorage {
+   OwnableStore      ownable;
+   address           _DODO_LOGIC_;
+   address           _CLONE_FACTORY_;
+
+   address _DEFAULT_SUPERVISOR_;
+   //_DODO_REGISTER_[baseToken][quoteToken] = dodo;
+   std::map<namesym, QuoteToken2Dodo> _DODO_REGISTER_;
+   std::vector<address>            _DODOs;
+
+   EOSLIB_SERIALIZE(
+       ZooStorage, (ownable)(_DODO_LOGIC_)(_CLONE_FACTORY_)(_DEFAULT_SUPERVISOR_)(_DODO_REGISTER_)(_DODOs))
+};
+
+typedef eosio::singleton<"zoo"_n, ZooStorage> ZooStorageSingleton;
+
+
+
